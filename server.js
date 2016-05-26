f5768e9433364c06e1813a7b61f9a1fb707e87f0
Ben Chidgey <ben@feedmypixel.com>
2016-05-26 14:41:13 +0100

    remove clean from npm scripts

--- server.js
+++ server.js
@@ -44,8 +44,7 @@ app.use('/public', express.static(__dirname + '/public'));
 app.use('/public', express.static(__dirname + '/govuk_modules/govuk_template/assets'));
 app.use('/public', express.static(__dirname + '/govuk_modules/govuk_frontend_toolkit'));
 app.use('/public/images/icons', express.static(__dirname + '/govuk_modules/govuk_frontend_toolkit/images'));
-app.use('/public/images', express.static(__dirname + '/hmrc_modules/images'));
-app.use('/public/javascripts', express.static(__dirname + '/hmrc_modules/javascripts'));
+app.use('/public', express.static(__dirname + '/hmrc_modules'));
 
 // Elements refers to icon folder instead of images folder
 app.use(favicon(path.join(__dirname, 'govuk_modules', 'govuk_template', 'assets', 'images','favicon.ico')));
