2b0d61b87115ff0633a3aaa38c6f2ad6f65367fc
Rory Powis <rory@hikkendry.com>
2018-09-05 12:30:44 +0200

    Move to extensions file

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ const documentationRoutes = require('./docs/documentation_routes.js')
 const packageJson = require('./package.json')
 const routes = require('./app/routes.js')
 const utils = require('./lib/utils.js')
-const pluginDetection = require('./lib/plugin-detection.js')
+const extensions = require('./lib/extensions.js')
+const extensionsConfig = extensions.getConfig()
 
 // Variables for v6 backwards compatibility
 // Set false by default, then turn on if we find /app/v6/routes.js
@@ -29,18 +30,18 @@ var v6App
 var v6Routes
 
 try {
-  v6Routes = require('./app/v6/routes.js')
-  useV6 = true
+	v6Routes = require('./app/v6/routes.js')
+	useV6 = true
 } catch (e) {
-  // No routes.js in app/v6 so we can continue with useV6 false
+	// No routes.js in app/v6 so we can continue with useV6 false
 }
 
 const app = express()
 const documentationApp = express()
 
 if (useV6) {
-  console.log('/app/v6/routes.js detected - using v6 compatibility mode')
-  v6App = express()
+	console.log('/app/v6/routes.js detected - using v6 compatibility mode')
+	v6App = express()
 }
 
 // Set cookies for use in cookie banner.
@@ -79,26 +80,30 @@ if (!useDocumentation) promoMode = 'false'
 // asking for username/password twice (for `http`, then `https`).
 var isSecure = (env === 'production' && useHttps === 'true')
 if (isSecure) {
-  app.use(utils.forceHttps)
-  app.set('trust proxy', 1) // needed for secure cookies on heroku
+	app.use(utils.forceHttps)
+	app.set('trust proxy', 1) // needed for secure cookies on heroku
 }
 
 // Ask for username and password on production
 if (env === 'production' && useAuth === 'true') {
-  app.use(utils.basicAuth(username, password))
+	app.use(utils.basicAuth(username, password))
 }
 
 // Set up App
-var appViews = pluginDetection.getList('nunjucksDirs', pluginDetection.transform.scopeFilePathsToModule).reverse().concat([
-  path.join(__dirname, '/app/views/'),
-  path.join(__dirname, '/lib/')
-])
+var appViews = [
+	path.join(__dirname, '/node_modules/govuk-frontend/'),
+	path.join(__dirname, '/node_modules/govuk-frontend/components')
+].concat(
+	extensionsConfig.nunjucksPaths.reverse(),
+	path.join(__dirname, '/app/views/'),
+	path.join(__dirname, '/lib/')
+)
 
 var nunjucksAppEnv = nunjucks.configure(appViews, {
-  autoescape: true,
-  express: app,
-  noCache: true,
-  watch: true
+	autoescape: true,
+	express: app,
+	noCache: true,
+	watch: true
 })
 
 // Add Nunjucks filters
@@ -109,86 +114,96 @@ app.set('view engine', 'html')
 
 // Middleware to serve static assets
 app.use('/public', express.static(path.join(__dirname, '/public')))
+app.use('/assets', express.static(path.join(__dirname, 'node_modules', 'govuk-frontend', 'assets')))
 
-app.use((req, res, next) => {
-  res.locals = res.locals || {}
-  Object.assign(res.locals, {
-    pluginConfig: {
-      scripts: pluginDetection.getList('scripts', pluginDetection.transform.publicAssetPaths),
-      stylesheets: pluginDetection.getList('stylesheets', pluginDetection.transform.publicAssetPaths)
-    }
-  })
-  next()
+// Serve govuk-frontend in /node_modules
+app.use('/node_modules/govuk-frontend', express.static(path.join(__dirname, '/node_modules/govuk-frontend')))
+
+// Serve extensions in /node_modules
+extensions.getMappedPaths(extensionsConfig.scripts).concat(
+	extensions.getMappedPaths(extensionsConfig.stylesheets),
+	extensions.getMappedPaths(extensionsConfig.assets),
+	extensions.getMappedPaths(extensionsConfig.globalAssets)
+).forEach(paths => {
+	app.use(paths.publicPath, express.static(paths.filesystemPath))
 })
 
-// Serve govuk-frontend in /public
-pluginDetection.getList('scripts', pluginDetection.transform.filesystemPathAndPublicAssetPaths)
-  .concat(pluginDetection.getList('stylesheets', pluginDetection.transform.filesystemPathAndPublicAssetPaths))
-  .concat(pluginDetection.getList('assets', pluginDetection.transform.filesystemPathAndPublicAssetPaths).reverse())
-  .concat(pluginDetection.getList('globalAssets', pluginDetection.transform.filesystemPathAndGlobalAssetPaths).reverse())
-  .forEach(paths => {
-    app.use(paths.publicPath, express.static(paths.filesystemPath))
-  })
+app.use((req, res, next) => {
+	res.locals = res.locals || {}
+
+	Object.assign(res.locals, {
+		pluginConfig: {
+			scripts: extensions.getPublicPaths(extensionsConfig.scripts),
+			stylesheets: extensions.getPublicPaths(extensionsConfig.scripts)
+		}
+	})
+
+	next()
+})
 
 // Set up documentation app
 if (useDocumentation) {
-  var documentationViews = pluginDetection.getList('nunjucksDirs', pluginDetection.transform.scopeFilePathsToModule).concat([
-    path.join(__dirname, '/docs/views/'),
-    path.join(__dirname, '/lib/')
-  ])
+	var documentationViews = [
+		path.join(__dirname, '/node_modules/govuk-frontend/'),
+		path.join(__dirname, '/node_modules/govuk-frontend/components')
+	].concat(
+		extensionsConfig.nunjucksPaths,
+		path.join(__dirname, '/docs/views/'),
+		path.join(__dirname, '/lib/')
+	)
 
-  var nunjucksDocumentationEnv = nunjucks.configure(documentationViews, {
-    autoescape: true,
-    express: documentationApp,
-    noCache: true,
-    watch: true
-  })
-  // Nunjucks filters
-  utils.addNunjucksFilters(nunjucksDocumentationEnv)
+	var nunjucksDocumentationEnv = nunjucks.configure(documentationViews, {
+		autoescape: true,
+		express: documentationApp,
+		noCache: true,
+		watch: true
+	})
+	// Nunjucks filters
+	utils.addNunjucksFilters(nunjucksDocumentationEnv)
 
-  // Set views engine
-  documentationApp.set('view engine', 'html')
+	// Set views engine
+	documentationApp.set('view engine', 'html')
 }
 
 // Support for parsing data in POSTs
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
-  extended: true
+	extended: true
 }))
 
 // Set up v6 app for backwards compatibility
 if (useV6) {
-  var v6Views = [
-    path.join(__dirname, '/node_modules/govuk_template_jinja/views/layouts'),
-    path.join(__dirname, '/app/v6/views/'),
-    path.join(__dirname, '/lib/v6') // for old unbranded template
-  ]
+	var v6Views = [
+		path.join(__dirname, '/node_modules/govuk_template_jinja/views/layouts'),
+		path.join(__dirname, '/app/v6/views/'),
+		path.join(__dirname, '/lib/v6') // for old unbranded template
+	]
 
-  var nunjucksV6Env = nunjucks.configure(v6Views, {
-    autoescape: true,
-    express: v6App,
-    noCache: true,
-    watch: true
-  })
-  // Nunjucks filters
-  utils.addNunjucksFilters(nunjucksV6Env)
+	var nunjucksV6Env = nunjucks.configure(v6Views, {
+		autoescape: true,
+		express: v6App,
+		noCache: true,
+		watch: true
+	})
+	// Nunjucks filters
+	utils.addNunjucksFilters(nunjucksV6Env)
 
-  // Set views engine
-  v6App.set('view engine', 'html')
+	// Set views engine
+	v6App.set('view engine', 'html')
 
-  // Backward compatibility with GOV.UK Elements
-  app.use('/public/v6/', express.static(path.join(__dirname, '/node_modules/govuk_template_jinja/assets')))
-  app.use('/public/v6/', express.static(path.join(__dirname, '/node_modules/govuk_frontend_toolkit')))
-  app.use('/public/v6/javascripts/govuk/', express.static(path.join(__dirname, '/node_modules/govuk_frontend_toolkit/javascripts/govuk/')))
+	// Backward compatibility with GOV.UK Elements
+	app.use('/public/v6/', express.static(path.join(__dirname, '/node_modules/govuk_template_jinja/assets')))
+	app.use('/public/v6/', express.static(path.join(__dirname, '/node_modules/govuk_frontend_toolkit')))
+	app.use('/public/v6/javascripts/govuk/', express.static(path.join(__dirname, '/node_modules/govuk_frontend_toolkit/javascripts/govuk/')))
 }
 
 // Add global variable to determine if DoNotTrack is enabled.
 // This indicates a user has explicitly opted-out of tracking.
 // Therefore we can avoid injecting third-party scripts that do not respect this decision.
 app.use(function (req, res, next) {
-  // See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/DNT
-  res.locals.doNotTrackEnabled = (req.header('DNT') === '1')
-  next()
+	// See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/DNT
+	res.locals.doNotTrackEnabled = (req.header('DNT') === '1')
+	next()
 })
 
 // Add variables that are available in all views
@@ -202,148 +217,148 @@ app.locals.serviceName = config.serviceName
 
 // Support session data
 app.use(session({
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 4, // 4 hours
-    secure: isSecure
-  },
-  // use random name to avoid clashes with other prototypes
-  name: 'govuk-prototype-kit-' + crypto.randomBytes(64).toString('hex'),
-  resave: false,
-  saveUninitialized: false,
-  secret: crypto.randomBytes(64).toString('hex')
+	cookie: {
+		maxAge: 1000 * 60 * 60 * 4, // 4 hours
+		secure: isSecure
+	},
+	// use random name to avoid clashes with other prototypes
+	name: 'govuk-prototype-kit-' + crypto.randomBytes(64).toString('hex'),
+	resave: false,
+	saveUninitialized: false,
+	secret: crypto.randomBytes(64).toString('hex')
 }))
 
 // Automatically store all data users enter
 if (useAutoStoreData === 'true') {
-  app.use(utils.autoStoreData)
-  utils.addCheckedFunction(nunjucksAppEnv)
-  if (useDocumentation) {
-    utils.addCheckedFunction(nunjucksDocumentationEnv)
-  }
-  if (useV6) {
-    utils.addCheckedFunction(nunjucksV6Env)
-  }
+	app.use(utils.autoStoreData)
+	utils.addCheckedFunction(nunjucksAppEnv)
+	if (useDocumentation) {
+		utils.addCheckedFunction(nunjucksDocumentationEnv)
+	}
+	if (useV6) {
+		utils.addCheckedFunction(nunjucksV6Env)
+	}
 }
 
 // Clear all data in session if you open /prototype-admin/clear-data
 app.get('/prototype-admin/clear-data', function (req, res) {
-  req.session.destroy()
-  res.render('prototype-admin/clear-data')
+	req.session.destroy()
+	res.render('prototype-admin/clear-data')
 })
 
 // Redirect root to /docs when in promo mode.
 if (promoMode === 'true') {
-  console.log('Prototype Kit running in promo mode')
+	console.log('Prototype Kit running in promo mode')
 
-  app.locals.cookieText = 'GOV.UK uses cookies to make the site simpler. <a href="/docs/cookies">Find out more about cookies</a>'
+	app.locals.cookieText = 'GOV.UK uses cookies to make the site simpler. <a href="/docs/cookies">Find out more about cookies</a>'
 
-  app.get('/', function (req, res) {
-    res.redirect('/docs')
-  })
+	app.get('/', function (req, res) {
+		res.redirect('/docs')
+	})
 
-  // Allow search engines to index the Prototype Kit promo site
-  app.get('/robots.txt', function (req, res) {
-    res.type('text/plain')
-    res.send('User-agent: *\nAllow: /')
-  })
+	// Allow search engines to index the Prototype Kit promo site
+	app.get('/robots.txt', function (req, res) {
+		res.type('text/plain')
+		res.send('User-agent: *\nAllow: /')
+	})
 } else {
-  // Prevent search indexing
-  app.use(function (req, res, next) {
-    // Setting headers stops pages being indexed even if indexed pages link to them.
-    res.setHeader('X-Robots-Tag', 'noindex')
-    next()
-  })
+	// Prevent search indexing
+	app.use(function (req, res, next) {
+		// Setting headers stops pages being indexed even if indexed pages link to them.
+		res.setHeader('X-Robots-Tag', 'noindex')
+		next()
+	})
 
-  app.get('/robots.txt', function (req, res) {
-    res.type('text/plain')
-    res.send('User-agent: *\nDisallow: /')
-  })
+	app.get('/robots.txt', function (req, res) {
+		res.type('text/plain')
+		res.send('User-agent: *\nDisallow: /')
+	})
 }
 
 // Load routes (found in app/routes.js)
 if (typeof (routes) !== 'function') {
-  console.log(routes.bind)
-  console.log('Warning: the use of bind in routes is deprecated - please check the Prototype Kit documentation for writing routes.')
-  routes.bind(app)
+	console.log(routes.bind)
+	console.log('Warning: the use of bind in routes is deprecated - please check the Prototype Kit documentation for writing routes.')
+	routes.bind(app)
 } else {
-  app.use('/', routes)
+	app.use('/', routes)
 }
 
 if (useDocumentation) {
-  // Clone app locals to documentation app locals
-  // Use Object.assign to ensure app.locals is cloned to prevent additions from
-  // updating the original app.locals
-  documentationApp.locals = Object.assign({}, app.locals)
-  documentationApp.locals.serviceName = 'Prototype Kit'
+	// Clone app locals to documentation app locals
+	// Use Object.assign to ensure app.locals is cloned to prevent additions from
+	// updating the original app.locals
+	documentationApp.locals = Object.assign({}, app.locals)
+	documentationApp.locals.serviceName = 'Prototype Kit'
 
-  // Create separate router for docs
-  app.use('/docs', documentationApp)
+	// Create separate router for docs
+	app.use('/docs', documentationApp)
 
-  // Docs under the /docs namespace
-  documentationApp.use('/', documentationRoutes)
+	// Docs under the /docs namespace
+	documentationApp.use('/', documentationRoutes)
 }
 
 if (useV6) {
-  // Clone app locals to v6 app locals
-  v6App.locals = Object.assign({}, app.locals)
-  v6App.locals.asset_path = '/public/v6/'
+	// Clone app locals to v6 app locals
+	v6App.locals = Object.assign({}, app.locals)
+	v6App.locals.asset_path = '/public/v6/'
 
-  // Create separate router for v6
-  app.use('/', v6App)
+	// Create separate router for v6
+	app.use('/', v6App)
 
-  // Docs under the /docs namespace
-  v6App.use('/', v6Routes)
+	// Docs under the /docs namespace
+	v6App.use('/', v6Routes)
 }
 
 // Strip .html and .htm if provided
 app.get(/\.html?$/i, function (req, res) {
-  var path = req.path
-  var parts = path.split('.')
-  parts.pop()
-  path = parts.join('.')
-  res.redirect(path)
+	var path = req.path
+	var parts = path.split('.')
+	parts.pop()
+	path = parts.join('.')
+	res.redirect(path)
 })
 
 // Auto render any view that exists
 
 // App folder routes get priority
 app.get(/^([^.]+)$/, function (req, res, next) {
-  utils.matchRoutes(req, res, next)
+	utils.matchRoutes(req, res, next)
 })
 
 if (useDocumentation) {
-  // Documentation  routes
-  documentationApp.get(/^([^.]+)$/, function (req, res, next) {
-    if (!utils.matchMdRoutes(req, res)) {
-      utils.matchRoutes(req, res, next)
-    }
-  })
+	// Documentation  routes
+	documentationApp.get(/^([^.]+)$/, function (req, res, next) {
+		if (!utils.matchMdRoutes(req, res)) {
+			utils.matchRoutes(req, res, next)
+		}
+	})
 }
 
 if (useV6) {
-  // App folder routes get priority
-  v6App.get(/^([^.]+)$/, function (req, res, next) {
-    utils.matchRoutes(req, res, next)
-  })
+	// App folder routes get priority
+	v6App.get(/^([^.]+)$/, function (req, res, next) {
+		utils.matchRoutes(req, res, next)
+	})
 }
 
 // Redirect all POSTs to GETs - this allows users to use POST for autoStoreData
 app.post(/^\/([^.]+)$/, function (req, res) {
-  res.redirect('/' + req.params[0])
+	res.redirect('/' + req.params[0])
 })
 
 // Catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  var err = new Error(`Page not found: ${req.path}`)
-  err.status = 404
-  next(err)
+	var err = new Error(`Page not found: ${req.path}`)
+	err.status = 404
+	next(err)
 })
 
 // Display error
 app.use(function (err, req, res, next) {
-  console.error(err.message)
-  res.status(err.status || 500)
-  res.send(err.message)
+	console.error(err.message)
+	res.status(err.status || 500)
+	res.send(err.message)
 })
 
 console.log('\nGOV.UK Prototype Kit v' + releaseVersion)
@@ -351,23 +366,23 @@ console.log('\nNOTICE: the kit is for building prototypes, do not use it for pro
 
 // Find a free port and start the server
 utils.findAvailablePort(app, function (port) {
-  console.log('Listening on port ' + port + '   url: http://localhost:' + port)
-  if (env === 'production' || useBrowserSync === 'false') {
-    app.listen(port)
-  } else {
-    app.listen(port - 50, function () {
-      browserSync({
-        proxy: 'localhost:' + (port - 50),
-        port: port,
-        ui: false,
-        files: ['public/**/*.*', 'app/views/**/*.*'],
-        ghostmode: false,
-        open: false,
-        notify: false,
-        logLevel: 'error'
-      })
-    })
-  }
+	console.log('Listening on port ' + port + '   url: http://localhost:' + port)
+	if (env === 'production' || useBrowserSync === 'false') {
+		app.listen(port)
+	} else {
+		app.listen(port - 50, function () {
+			browserSync({
+				proxy: 'localhost:' + (port - 50),
+				port: port,
+				ui: false,
+				files: ['public/**/*.*', 'app/views/**/*.*'],
+				ghostmode: false,
+				open: false,
+				notify: false,
+				logLevel: 'error'
+			})
+		})
+	}
 })
 
 module.exports = app
