5b74dd644d31d83a17315e8bfcbcdf1e9f845e57
Mat Carey <mat+yagni-digital@matcarey.co.uk>
2018-09-26 15:51:49 +0100

    Expecting the next version of govuk-frontend to contain a config file.

--- app/config.js
+++ app/config.js
@@ -33,17 +33,6 @@ module.exports = {
 
   // These extensions will be included before others
   // This is where you can deal with extensions which conflict or rely on each other
-  foundationExtensions: ['govuk-frontend'],
-
-  // If a node_module doesn't have a config file but can be used as an extension you can write your own config here
-  // if the module has an extension config included this will be ignored
-  additionalExtensionConfigs: {
-    'govuk-frontend': {
-      nunjucksPaths: ['/', '/components'],
-      scripts: ['/all.js'],
-      assets: [{global: true, src: '/assets'}],
-      sass: ['/all.scss']
-    }
-  }
+  foundationExtensions: ['govuk-frontend']
 
 }
